f550a2a545b721dc45811c9bdee427219e83aaac
bengisukayan <bengisukayan@gmail.com>
2024-07-30 22:37:48 +0300

    Connected interactions to network and tested

--- Assets/Scripts/Interaction/BoxInteract.cs
+++ Assets/Scripts/Interaction/BoxInteract.cs
@@ -1,11 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class BoxInteract : MonoBehaviour, IInteractable
+public class BoxInteract : NetworkBehaviour, IInteractable
 {
-    public void Interact() {
-        gameObject.GetComponent<Renderer>().material.color = Color.red;
-        Debug.Log("Red");
+    public void Interact()
+    {
+        if (IsServer)
+        {
+            ChangeColorClientRpc(Color.red);
+        }
+        else
+        {
+            ChangeColorServerRpc(Color.red);
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void ChangeColorServerRpc(Color color)
+    {
+        ChangeColorClientRpc(color);
+    }
+
+    [ClientRpc]
+    private void ChangeColorClientRpc(Color color)
+    {
+        gameObject.GetComponent<Renderer>().material.color = color;
+        Debug.Log("Color changed to red for all clients");
     }
 }
